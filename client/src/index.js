ce0f5b5f1c0f8190034e071159b308c3c74b2d25
justinjeong <justin.jeong5@gmail.com>
2020-12-26 13:41:13 +0900

    add: HashRouter

--- client/src/index.js
+++ client/src/index.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import { BrowserRouter } from 'react-router-dom'
+import { HashRouter } from 'react-router-dom'
 import { Provider } from 'react-redux'
 
 import './index.css';
@@ -13,9 +13,9 @@ import store from './store/storeConfiguration'
 ReactDOM.render(
   <>
     <Provider store={store}>
-      <BrowserRouter>
+      <HashRouter>
         <App />
-      </BrowserRouter>
+      </HashRouter>
     </Provider>
   </>,
   document.getElementById('root')
