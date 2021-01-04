76307e409e411145fbed004223f344c99cf440ea
justinjeong <justin.jeong5@gmail.com>
2021-01-04 16:01:08 +0900

    refactor: redux-devtools when development only

--- client/src/store/storeConfiguration.js
+++ client/src/store/storeConfiguration.js
@@ -1,4 +1,4 @@
-import { applyMiddleware, createStore } from 'redux';
+import { applyMiddleware, compose, createStore } from 'redux';
 import createSagaMiddleware from 'redux-saga';
 import { composeWithDevTools } from 'redux-devtools-extension';
 
@@ -12,8 +12,10 @@ const loggerMiddleware = ({ dispatch, getState }) => (next) => (action) => {
 
 const sagaMiddleware = createSagaMiddleware();
 const middlewares = [sagaMiddleware, loggerMiddleware];
-const enhancer = composeWithDevTools(applyMiddleware(...middlewares));
-const store = createStore(rootReducer, enhancer, window.__REDUX_DEVTOOLS_EXTIONSION__ && window.__REDUX_DEVTOOLS_EXTIONSION__());
+const enhancer = process.env.NODE_ENV === 'production'
+  ? compose(applyMiddleware(...middlewares))
+  : composeWithDevTools(applyMiddleware(...middlewares))
+const store = createStore(rootReducer, enhancer);
 store.sagaTask = sagaMiddleware.run(rootSaga);
 
 export default store;
