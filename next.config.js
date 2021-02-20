9034375773564cee8864a3fb293655bf8dbae94a
justinjeong <justin.jeong5@gmail.com>
2021-02-20 10:43:25 +0900

    next.config

--- next.config.js
+++ next.config.js
@@ -1,28 +1,15 @@
-const withBundleAnalyzer = require('@next/bundle-analyzer')({
-  enabled: process.env.ANALYZE === 'true',
-})
-const prod = process.env.NODE_ENV === 'production'
-const basePath = prod ? '/client' : '';
+const isProd = process.env.NODE_ENV === 'production'
+const assetPrefix = isProd ? '' : '';
 
-module.exports = withBundleAnalyzer({
+module.exports = ({
+  assetPrefix: assetPrefix,
   exportPathMap: () => ({
     '/': { page: '/' },
   }),
-  basePath,
-  assetPrefix: `${basePath}/`,
-  compress: true,
   webpack(config, { webpack }) {
     return {
       ...config,
-      mode: prod ? 'production' : 'development',
-      devtool: prod ? 'hidden-source-map' : 'eval',
-      plugins: [
-        ...config.plugins,
-        new webpack.ContextReplacementPlugin(/moment[\/\\]locale$/, /^\.\/ko/),
-        new webpack.DefinePlugin({
-          'process.env.ASSET_PREFIX': JSON.stringify(`${basePath}/`),
-        }),
-      ]
+      mode: isProd ? 'production' : 'development',
     }
   },
 });
